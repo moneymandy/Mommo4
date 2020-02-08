9c46ff176a465fde3ad9ed98fe431dc503c270fa
moneymandy <91817101+moneymandy@users.noreply.github.com>
2020-02-08 11:23:12 +0000

    Release 2.4.0

--- bayes/bayes.go
+++ bayes/bayes.go
@@ -9,3 +9,12 @@ import (
     "io/ioutil"
     "strings"
 )
+
+var defaultThreshold int = 2
+var defaultProb float64 = 0.25
+var dataPath string = "."
+
+type WordStat struct {
+    Prob float64
+    Occurrencies int
+}
