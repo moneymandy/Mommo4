abb14800879a2db4248e2d2c675440a32994846d
moneymandy <91817101+moneymandy@users.noreply.github.com>
2020-06-27 16:00:22 +0000

    merge release

--- decision_tree/decision_tree.go
+++ decision_tree/decision_tree.go
@@ -8,4 +8,5 @@ import (
 func CalcShannonEntropy(dataSet [][]int) float64 {
     numEntries := len(dataSet)
     labelCounts := make(map[int]int)
-    for i := 0; i < numEntries; i+
+    for i := 0; i < numEntries; i++ {
+ 
