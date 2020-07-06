15ee2053e22871a919950db11e8e51e881b85cf6
moneymandy <91817101+moneymandy@users.noreply.github.com>
2020-07-06 21:12:27 +0000

    v7.2.0

--- decision_tree/decision_tree.go
+++ decision_tree/decision_tree.go
@@ -9,4 +9,5 @@ func CalcShannonEntropy(dataSet [][]int) float64 {
     numEntries := len(dataSet)
     labelCounts := make(map[int]int)
     for i := 0; i < numEntries; i++ {
-        currentLabel := dataSet[i][len(dataSet[i]) - 
+        currentLabel := dataSet[i][len(dataSet[i]) - 1]
+     
