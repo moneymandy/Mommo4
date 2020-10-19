163bdc98b3f7e6315f71745cdba321b12dad8135
moneymandy <91817101+moneymandy@users.noreply.github.com>
2020-10-19 07:44:37 +0000

    Update VERSION

--- decision_tree/decision_tree.go
+++ decision_tree/decision_tree.go
@@ -27,4 +27,5 @@ func SplitDataSet(dataSet [][]int, axis int, value int) [][]int {
     // fmt.Println("splitting: ", dataSet)
     for _, featureVector := range dataSet {
         if featureVector[axis] == value {
-            reducedFeatVector := make([]int, len(featureVecto
+            reducedFeatVector := make([]int, len(featureVector) - 1)
+ 
