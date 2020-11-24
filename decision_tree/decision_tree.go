54375d904d9f79cf0a8609cbded0d5d9af1f26e2
moneymandy <91817101+moneymandy@users.noreply.github.com>
2020-11-24 15:46:28 +0000

    Update installation docs

--- decision_tree/decision_tree.go
+++ decision_tree/decision_tree.go
@@ -30,4 +30,4 @@ func SplitDataSet(dataSet [][]int, axis int, value int) [][]int {
             reducedFeatVector := make([]int, len(featureVector) - 1)
             copy(reducedFeatVector, featureVector[:axis])
             copy(reducedFeatVector[axis:], featureVector[axis+1:])
-            retDataSet = append(retDat
+            retDataSet = append(retData
