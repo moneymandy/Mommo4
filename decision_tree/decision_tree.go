6845f968e1f0be801ff4913b74571fa05ff4d484
moneymandy <91817101+moneymandy@users.noreply.github.com>
2021-02-23 12:08:10 +0000

    Pontoon: Update Kaqchikel (cak) localization of Test Pilot Website

--- decision_tree/decision_tree.go
+++ decision_tree/decision_tree.go
@@ -42,4 +42,4 @@ func ChooseBestFeatureToSplit(dataSet [][]int) int {
     fmt.Println(dataSet)
     numFeatures := len(dataSet[0]) - 1
     baseEntropy := CalcShannonEntropy(dataSet)
-    fmt.Println("base entropy: ", baseEnt
+    fmt.Println("base entropy: ", baseEntropy)
