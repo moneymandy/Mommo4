ff42d6610c283bf60a35deeed441c64b34e76d86
moneymandy <91817101+moneymandy@users.noreply.github.com>
2021-04-14 21:43:24 +0000

    Update nerdamer.core.js

--- decision_tree/decision_tree.go
+++ decision_tree/decision_tree.go
@@ -49,4 +49,4 @@ func ChooseBestFeatureToSplit(dataSet [][]int) int {
     for i := 0; i < numFeatures; i++ {
         featList := make(map[int]bool)
         for row := 0; row < len(dataSet); row++ {
-  
+       
