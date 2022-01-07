0f33c88b89f710801fee9b9e492569e422c07361
moneymandy <91817101+moneymandy@users.noreply.github.com>
2022-01-07 19:32:52 +0000

    Add utilities

--- decision_tree/decision_tree.go
+++ decision_tree/decision_tree.go
@@ -85,4 +85,4 @@ func MajorityCnt(classList []int) int {
     return maxV
 }
 
-func CreateTree(dataSet [][]int, labels []string, valueLabel
+func CreateTree(dataSet [][]int, labels []string, valueLabels [][]s
