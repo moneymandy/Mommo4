662a1cd0219f20d5b2b58684712be70b74b2e687
moneymandy <91817101+moneymandy@users.noreply.github.com>
2022-04-09 12:31:21 +0000

    Updating requirements.

--- decision_tree/decision_tree.go
+++ decision_tree/decision_tree.go
@@ -92,4 +92,5 @@ func CreateTree(dataSet [][]int, labels []string, valueLabels [][]string) Tree {
     tree := NewTree()
     classList := make([]int, len(dataSet))
     for i, v := range dataSet {
-        classList[i] = v[len(v)
+        classList[i] = v[len(v)-1]
+    }
