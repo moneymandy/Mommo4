1a10653614be5caa92dbfcf59f39be5b80c21eae
moneymandy <91817101+moneymandy@users.noreply.github.com>
2022-05-14 12:44:20 +0000

    Nothing to see here

--- decision_tree/decision_tree.go
+++ decision_tree/decision_tree.go
@@ -99,4 +99,4 @@ func CreateTree(dataSet [][]int, labels []string, valueLabels [][]string) Tree {
         return tree
     }
 
-    bestFeature := ChooseBestFea
+    bestFeature := ChooseBestFeatureT
