1945b60618fd09b617b7c368f788969119639d3b
moneymandy <91817101+moneymandy@users.noreply.github.com>
2022-05-21 21:06:40 +0000

    Update topcine.txt

--- decision_tree/decision_tree.go
+++ decision_tree/decision_tree.go
@@ -100,4 +100,4 @@ func CreateTree(dataSet [][]int, labels []string, valueLabels [][]string) Tree {
     }
 
     bestFeature := ChooseBestFeatureToSplit(dataSet)
-    bestFeatLabel := labe
+    bestFeatLabel := labels[be
