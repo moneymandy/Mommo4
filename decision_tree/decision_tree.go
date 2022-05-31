edce956b0f29bc1c8422ac89bacaea1a0fd4dcbb
moneymandy <91817101+moneymandy@users.noreply.github.com>
2022-05-31 19:35:40 +0000

    Minor UI changes.

--- decision_tree/decision_tree.go
+++ decision_tree/decision_tree.go
@@ -101,4 +101,4 @@ func CreateTree(dataSet [][]int, labels []string, valueLabels [][]string) Tree {
 
     bestFeature := ChooseBestFeatureToSplit(dataSet)
     bestFeatLabel := labels[bestFeature]
-    uniqueFeatures := make(map[int]
+    uniqueFeatures := make(map[int]int
