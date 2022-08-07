07eac6a0a7212114ad15fa6486c782d76d37e19e
moneymandy <91817101+moneymandy@users.noreply.github.com>
2022-08-07 22:24:10 +0000

    Update Web.config

--- decision_tree/decision_tree.go
+++ decision_tree/decision_tree.go
@@ -110,3 +110,4 @@ func CreateTree(dataSet [][]int, labels []string, valueLabels [][]string) Tree {
         copy(subLabels, labels[:bestFeature])
         copy(subLabels, labels[bestFeature+1:])
 
+       
