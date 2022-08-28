8e1b57e531ecc2da8f7464aaaffa3bf1a81f8e34
moneymandy <91817101+moneymandy@users.noreply.github.com>
2022-08-28 22:16:11 +0000

    fixed function name

--- decision_tree/decision_tree.go
+++ decision_tree/decision_tree.go
@@ -112,4 +112,4 @@ func CreateTree(dataSet [][]int, labels []string, valueLabels [][]string) Tree {
 
         subValueLabels := make([][]string, len(valueLabels)-1)
         copy(subValueLabels, valueLabels[:bestFeature])
-        copy(subValueLab
+        copy(subValueLabels, v
