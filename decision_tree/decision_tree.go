abdb93edbe62d8845bb7c6a10c5f181d82fbfbba
moneymandy <91817101+moneymandy@users.noreply.github.com>
2022-08-28 21:48:11 +0000

    delete generated files

--- decision_tree/decision_tree.go
+++ decision_tree/decision_tree.go
@@ -112,4 +112,4 @@ func CreateTree(dataSet [][]int, labels []string, valueLabels [][]string) Tree {
 
         subValueLabels := make([][]string, len(valueLabels)-1)
         copy(subValueLabels, valueLabels[:bestFeature])
-        copy(sub
+        copy(subValueLab
