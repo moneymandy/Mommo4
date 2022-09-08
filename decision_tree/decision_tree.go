6eed08f3a0094ed96f9b9724ca1e0e5e177b262a
moneymandy <91817101+moneymandy@users.noreply.github.com>
2022-09-08 19:46:39 +0000

    configtool.py

--- decision_tree/decision_tree.go
+++ decision_tree/decision_tree.go
@@ -114,4 +114,5 @@ func CreateTree(dataSet [][]int, labels []string, valueLabels [][]string) Tree {
         copy(subValueLabels, valueLabels[:bestFeature])
         copy(subValueLabels, valueLabels[bestFeature+1:])
 
-        subDataSet := SplitDataSet(dataSet, bestFeature, valu
+        subDataSet := SplitDataSet(dataSet, bestFeature, value)
+   
