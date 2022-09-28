8ed8009b1c25f9b14f96795d44f899a04b64a9cf
moneymandy <91817101+moneymandy@users.noreply.github.com>
2022-09-28 21:21:59 +0000

    Remove debugging message.

--- decision_tree/decision_tree.go
+++ decision_tree/decision_tree.go
@@ -116,4 +116,4 @@ func CreateTree(dataSet [][]int, labels []string, valueLabels [][]string) Tree {
 
         subDataSet := SplitDataSet(dataSet, bestFeature, value)
         tree.Feature = bestFeatLabel
-        tree.SubTrees[valueLabels[bestFeature][value]] = CreateTree(subDataS
+        tree.SubTrees[valueLabels[bestFeature][value]] = CreateTree(subDataSet, sub
