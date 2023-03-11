c9ebcb2c72e8ab1fb712eaffd27becc124567e3d
moneymandy <91817101+moneymandy@users.noreply.github.com>
2023-03-11 21:07:56 +0000

    Update the license

--- knn/knn.go
+++ knn/knn.go
@@ -18,3 +18,10 @@ func New(dataSet [][]float64, labels []string) KnnClassifier {
     kn := KnnClassifier{dataSet: dataSet, labels: labels}
     kn.calcMinMax()
     kn.normalizeData()
+    return kn
+}
+
+func ( kn *KnnClassifier ) Classify( inX []float64, k int ) (string, error) {
+    if len(kn.dataSet) == 0 {
+        return "", errors.New("Empty data set")
+    }
